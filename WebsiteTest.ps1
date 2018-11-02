6db5e3756c617bcf03d58f43441e6f6be4f11677
saumil.shah <saumil.shah@IN-IT1240>
2018-11-02 09:46:23 +0530

    changes

--- WebsiteTest.ps1
+++ WebsiteTest.ps1
@@ -1,20 +1,6 @@
 Configuration WebsiteTest {
 
   param ($MachineName)
-  param 
-  ( 
-        [Parameter(Mandatory)]
-        [String]$DomainName,
-
-        [Parameter(Mandatory)]
-        [System.Management.Automation.PSCredential]$Admincreds,
-
-		[Parameter(Mandatory)]
-        [System.Management.Automation.PSCredential]$myFirstUserCreds
-  ) 
-
-    Import-DscResource -ModuleName xActiveDirectory, xNetworking, xPendingReboot
-    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
 
   Node $MachineName
   {
@@ -37,76 +23,6 @@ Configuration WebsiteTest {
         Name = "Web-Mgmt-Console"
         Ensure = "Present"
     }
-
-	LocalConfigurationManager 
-        {
-            ActionAfterReboot = 'ContinueConfiguration'            
-            ConfigurationMode = 'ApplyOnly'            
-            RebootNodeIfNeeded = $true
-        }
-
-	    WindowsFeature DNS 
-        { 
-            Ensure = "Present" 
-            Name = "DNS"		
-        }
-
-        xDnsServerAddress DnsServerAddress 
-        { 
-            Address        = '127.0.0.1' 
-            InterfaceAlias = $InterfaceAlias
-            AddressFamily  = 'IPv4'
-	        DependsOn = "[WindowsFeature]DNS"
-        }
-
-		WindowsFeature RSAT
-        {
-            Ensure = "Present"
-            Name = "RSAT"
-        }
-
-        WindowsFeature ADDSInstall 
-        { 
-            Ensure = "Present" 
-            Name = "AD-Domain-Services"
-	        DependsOn="[WindowsFeature]DNS" 
-        } 
-         
-        xADDomain FirstDC 
-        {
-            DomainName = $DomainName
-            DomainAdministratorCredential = $DomainCreds
-            SafemodeAdministratorPassword = $DomainCreds
-            DatabasePath = "C:\NTDS"
-            LogPath = "C:\NTDS"
-            SysvolPath = "C:\SYSVOL"
-	        DependsOn = "[WindowsFeature]ADDSInstall","[xDnsServerAddress]DnsServerAddress"
-        } 
-
-		xWaitForADDomain DscForestWait
-        {
-            DomainName = $DomainName
-            DomainUserCredential = $DomainCreds
-            RetryCount = $RetryCount
-            RetryIntervalSec = $RetryIntervalSec
-            DependsOn = "[xADDomain]FirstDC"
-        } 
-
-        xADUser FirstUser 
-        { 
-            DomainName = $DomainName 
-            DomainAdministratorCredential = $DomainCreds 
-            UserName = $myFirstUserCreds.Username 
-            Password = $myFirstUserCreds
-            Ensure = "Present" 
-            DependsOn = "[xWaitForADDomain]DscForestWait" 
-        } 
-
-        xPendingReboot Reboot1
-        { 
-            Name = "RebootServer"
-            DependsOn = "[xWaitForADDomain]DscForestWait"
-        }
 
   }
 }
